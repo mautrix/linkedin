3830c33a3ed6b3b54105f187a2137e7e5737214f
Sumner Evans <sumner.evans@automattic.com>
2025-01-20 12:10:59 -0700

    dockerfile: update to be more in line with other mautrix repos
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,20 @@
-FROM alpine:3.20
+FROM golang:1-alpine3.21 AS builder
+
+RUN apk add --no-cache git ca-certificates build-base su-exec olm-dev
+
+COPY . /build
+WORKDIR /build
+RUN ./build.sh
+
+FROM alpine:3.21
 
 ENV UID=1337 \
     GID=1337
 
-RUN apk add --no-cache ffmpeg su-exec ca-certificates bash jq curl yq-go
+RUN apk add --no-cache ffmpeg su-exec ca-certificates olm bash jq yq curl
 
-ARG EXECUTABLE=./cmd/mautrix-linkedin
-COPY $EXECUTABLE /usr/bin/mautrix-linkedin
-COPY ./docker-run.sh /docker-run.sh
-ENV BRIDGEV2=1
+COPY --from=builder /build/mautrix-linkedin /usr/bin/mautrix-linkedin
+COPY --from=builder /build/docker-run.sh /docker-run.sh
 VOLUME /data
-WORKDIR /data
 
 CMD ["/docker-run.sh"]
--- docker-run.sh
+++ docker-run.sh
@@ -4,14 +4,18 @@ if [[ -z "$GID" ]]; then
 	GID="$UID"
 fi
 
-BINARY_NAME=/usr/bin/mautrix-linkedin
-
+# Define functions.
 function fixperms {
 	chown -R $UID:$GID /data
+
+	# /opt/mautrix-linkedin is read-only, so disable file logging if it's pointing there.
+	if [[ "$(yq e '.logging.writers[1].filename' /data/config.yaml)" == "./logs/mautrix-linkedin.log" ]]; then
+		yq -I4 e -i 'del(.logging.writers[1])' /data/config.yaml
+	fi
 }
 
 if [[ ! -f /data/config.yaml ]]; then
-	$BINARY_NAME -c /data/config.yaml -e
+	/usr/bin/mautrix-linkedin -c /data/config.yaml -e
 	echo "Didn't find a config file."
 	echo "Copied default config file to /data/config.yaml"
 	echo "Modify that config file to your liking."
@@ -20,7 +24,7 @@ if [[ ! -f /data/config.yaml ]]; then
 fi
 
 if [[ ! -f /data/registration.yaml ]]; then
-	$BINARY_NAME -g -c /data/config.yaml -r /data/registration.yaml
+	/usr/bin/mautrix-linkedin -g -c /data/config.yaml -r /data/registration.yaml || exit $?
 	echo "Didn't find a registration file."
 	echo "Generated one for you."
 	echo "See https://docs.mau.fi/bridges/general/registering-appservices.html on how to use it."
@@ -29,4 +33,4 @@ fi
 
 cd /data
 fixperms
-exec su-exec $UID:$GID $BINARY_NAME
+exec su-exec $UID:$GID /usr/bin/mautrix-linkedin
