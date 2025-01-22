f9a142672b318200d97725d7da6b58536017f64d
Sumner Evans <sumner.evans@automattic.com>
2025-01-21 19:14:17 -0700

    linkedingo/types: fix UserProfile
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- pkg/linkedingo/types/user_profile.go
+++ pkg/linkedingo/types/user_profile.go
@@ -1,11 +1,19 @@
 package types
 
+type VectorImage struct {
+	RootURL   string `json:"rootUrl,omitempty"`
+	Artifacts []any  `json:"artifacts,omitempty"`
+}
+
+type Picture struct {
+	VectorImage *VectorImage `json:"com.linkedin.common.VectorImage"`
+}
+
 type UserProfile struct {
 	FirstName        string `json:"firstName"`
 	LastName         string `json:"lastName"`
 	Occupation       string `json:"occupation"`
 	PublicIdentifier string `json:"publicIdentifier"`
-	Picture          string `json:"picture,omitempty"`
 	Memorialized     bool   `json:"memorialized"`
 
 	EntityUrn     string `json:"entityUrn"`
@@ -13,6 +21,8 @@ type UserProfile struct {
 	DashEntityUrn string `json:"dashEntityUrn"`
 
 	TrackingId string `json:"trackingId"`
+
+	Picture Picture `json:"picture,omitempty"`
 }
 
 type UserLoginProfile struct {
