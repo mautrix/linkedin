f474a73525a4246a39d4ae917b8f9bb4c4aea77d
Sumner Evans <sumner.evans@automattic.com>
2025-02-19 17:15:30 -0700

    connector: first sync of conversations
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- pkg/connector/sync.go
+++ pkg/connector/sync.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 
 	"github.com/rs/zerolog"
+	"go.mau.fi/util/ptr"
+	"maunium.net/go/mautrix/bridgev2"
+	"maunium.net/go/mautrix/bridgev2/database"
+	"maunium.net/go/mautrix/bridgev2/simplevent"
 )
 
 func (l *LinkedInClient) syncConversations(ctx context.Context) {
@@ -15,5 +19,30 @@ func (l *LinkedInClient) syncConversations(ctx context.Context) {
 		log.Error().Err(err).Msg("failed to fetch initial inbox state")
 		return
 	}
+
 	fmt.Printf("%+v\n", conversations)
+
+	for _, conv := range conversations.Elements {
+		fmt.Printf("conv=%+v\n", conv)
+
+		l.main.Bridge.QueueRemoteEvent(l.userLogin, &simplevent.ChatResync{
+			ChatInfo: ptr.Ptr(l.conversationToChatInfo(conv)),
+			EventMeta: simplevent.EventMeta{
+				Type: bridgev2.RemoteEventChatResync,
+				LogContext: func(c zerolog.Context) zerolog.Context {
+					return c.Str("update", "sync")
+				},
+				PortalKey:    l.makePortalKey(conv.EntityURN),
+				CreatePortal: true,
+			},
+			CheckNeedsBackfillFunc: func(ctx context.Context, latestMessage *database.Message) (bool, error) {
+				if latestMessage == nil {
+					return true, nil
+				}
+				return true, nil
+				// TODO
+				// return TopMessage > latestMessageID, nil
+			},
+		})
+	}
 }
