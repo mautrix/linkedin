832c7b385dc6b764375f98915df2c70c6062bc43
Abestanis <abestanis.gc@gmail.com>
2025-05-08 13:44:46 +0200

    Fix type of identifiers column during migration
    The `ghost.identifiers` column has the type `jsonb`,
    but this expression retuned `text`, resulting in the following error on migration:
    ```
    "pq: column \"identifiers\" is of type jsonb but expression is of type text"
    ```

--- cmd/mautrix-linkedin/legacymigrate.sql
+++ cmd/mautrix-linkedin/legacymigrate.sql
@@ -28,7 +28,7 @@ SELECT
     false, -- avatar_set (need to set it on the new case-insensitive ghost mxid)
     false, -- contact_info_set (need to set it on the new case-insensitive ghost mxid)
     false, -- is_bot
-    '["linkedin:' || li_member_urn || '"]', -- identifiers
+    ('["linkedin:' || li_member_urn || '"]')::jsonb, -- identifiers
     '{}' -- metadata
 FROM puppet_old;
 
