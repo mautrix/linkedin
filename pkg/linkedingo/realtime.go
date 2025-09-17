819091886bee4e6625503f377bed48f3db35004c
Ping Chen <remotenonsense@gmail.com>
2025-09-17 23:18:55 +0900

    handlelinkedin: handle conversationDeletesTopic

--- pkg/linkedingo/realtime.go
+++ pkg/linkedingo/realtime.go
@@ -76,11 +76,12 @@ type DecoratedEventPayload struct {
 }
 
 type DecoratedEventData struct {
-	Type                     string                    `json:"_type,omitempty"`
-	DecoratedMessage         *DecoratedMessage         `json:"doDecorateMessageMessengerRealtimeDecoration,omitempty"`
-	DecoratedTypingIndicator *DecoratedTypingIndicator `json:"doDecorateTypingIndicatorMessengerRealtimeDecoration,omitempty"`
-	DecoratedSeenReceipt     *DecoratedSeenReceipt     `json:"doDecorateSeenReceiptMessengerRealtimeDecoration,omitempty"`
-	DecoratedReactionSummary *DecoratedReactionSummary `json:"doDecorateRealtimeReactionSummaryMessengerRealtimeDecoration,omitempty"`
+	Type                        string                       `json:"_type,omitempty"`
+	DecoratedConversationDelete *DecoratedConversationDelete `json:"doDecorateConversationDeleteMessengerRealtimeDecoration,omitempty"`
+	DecoratedMessage            *DecoratedMessage            `json:"doDecorateMessageMessengerRealtimeDecoration,omitempty"`
+	DecoratedTypingIndicator    *DecoratedTypingIndicator    `json:"doDecorateTypingIndicatorMessengerRealtimeDecoration,omitempty"`
+	DecoratedSeenReceipt        *DecoratedSeenReceipt        `json:"doDecorateSeenReceiptMessengerRealtimeDecoration,omitempty"`
+	DecoratedReactionSummary    *DecoratedReactionSummary    `json:"doDecorateRealtimeReactionSummaryMessengerRealtimeDecoration,omitempty"`
 }
 
 func (c *Client) RealtimeConnect(ctx context.Context) error {
